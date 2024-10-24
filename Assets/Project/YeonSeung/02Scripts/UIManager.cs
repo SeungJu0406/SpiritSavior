0743792d2f08048c1af267fe2bf6b387d8bb5d9e
YoungsonMan <choi13570@naver.com>
2024-10-25 01:10:21 +0900

    [Feat] LifeCount 추가
    맞으면 깎이는 목숨스타일

--- Assets/Project/YeonSeung/02Scripts/UIManager.cs
+++ Assets/Project/YeonSeung/02Scripts/UIManager.cs
@@ -5,11 +5,54 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
+
+    private bool _isDead;
+
+    [Header ("HP Bar")]
     [SerializeField] float curHp;
     [SerializeField] float maxHp;
     [SerializeField] private Slider _hpBar;
+
+
+    [Header("Live Count")]
+    [SerializeField] GameObject[] lives;
+    [SerializeField] public int life; 
+
+    public void TakeDamage(int damage)
+    {
+        if (life >= 1)
+        {
+            life -= damage;
+
+            Destroy(lives[life].gameObject);
+            if(life < 1)
+            {
+                _isDead = true;
+            }
+        }
+    }
+    public void LifeCount()
+    {
+        /* 데미지 받는거말고 그냥 하트수만 하면
+        if (life < 1)
+        {
+            Destroy(lives[0].gameObject);
+        }
+        else if (life < 2)
+        {
+            Destroy(lives[1].gameObject);
+        }
+        else if (life < 3)
+        {
+            Destroy(lives[2].gameObject);
+        } 
+        */
+    }
+
+
     void Start()
     {
+        life = lives.Length;
         curHp = maxHp;
     }
 
@@ -17,5 +60,6 @@ public class UIManager : MonoBehaviour
     void Update()
     {
         _hpBar.value = curHp / maxHp;
+        //TakeDamage(1);
     }
 }
