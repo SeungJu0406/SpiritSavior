96616a999ac4b3e439bd97ddeb54c6fb5746ab95
SeungJu0406 <nss8164@gmail.com>
2024-10-24 22:13:00 +0900

    feat : 사운드 매니저 코드 작성

--- Assets/Project/NoSeungJu/Script/SceneField.cs
+++ Assets/Project/NoSeungJu/Script/SceneField.cs
@@ -9,15 +9,15 @@ using UnityEditor;
 [System.Serializable]
 public class SceneField
 {
-    [SerializeField] private Object sceneAsset;
+    [SerializeField] private Object _sceneAsset;
 
-    [SerializeField] private string sceneName = "";
+    [SerializeField] private string _sceneName = "";
 
-    public string SceneName { get { return sceneName; } }
+    public string SceneName { get { return _sceneName; } }
 
     public static implicit operator string(SceneField sceneField)
     {
-        return sceneField.sceneName;
+        return sceneField._sceneName;
     }
 }
 
@@ -28,8 +28,8 @@ public class SceneFieldPropertyDrawer : PropertyDrawer
     public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
     {
         EditorGUI.BeginProperty(position, GUIContent.none, property);
-        SerializedProperty sceneAsset = property.FindPropertyRelative("sceneAsset");
-        SerializedProperty sceneName = property.FindPropertyRelative("sceneName");
+        SerializedProperty sceneAsset = property.FindPropertyRelative("_sceneAsset");
+        SerializedProperty sceneName = property.FindPropertyRelative("_sceneName");
         position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
 
         if (sceneAsset != null)
---
+++ Assets/Project/NoSeungJu/Script/SoundManager.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance;
+
+    [SerializeField] private AudioSource _bgm;
+    [SerializeField] private AudioSource _sfx;
+
+    private void Awake()
+    {
+        if (Instance == null) Instance = this;
+        else Destroy(gameObject);
+    }
+
+    /// <summary>
+    /// BGM 실행
+    /// </summary>
+    /// <param name="clip"></param>
+    public void PlayBGM(AudioClip clip)
+    {
+        _bgm.clip = clip;
+        _bgm.Play();
+    }
+
+    /// <summary>
+    /// BGM 정지
+    /// </summary>
+    public void StopBGM() 
+    {
+        if (_bgm.isPlaying)
+        {
+            _bgm.Stop();
+        }
+    }
+
+    /// <summary>
+    /// BGM 일시정지
+    /// </summary>
+    public void PauseBGM()
+    {
+        if (_bgm.isPlaying)
+        {
+            _bgm.Pause();
+        }
+    }
+
+    /// <summary>
+    /// BGM 볼륨 조절
+    /// </summary>
+    /// <param name="volume"></param>
+
+    public void SetVolumeBGM(float volume)
+    {
+        _bgm.volume = volume;
+    }
+
+    /// <summary>
+    /// SFX 실행
+    /// </summary>
+    /// <param name="clip"></param>
+    public void PlaySFX(AudioClip clip) 
+    {
+        _sfx.PlayOneShot(clip);
+    }
+
+    /// <summary>
+    /// SFX 볼륨 조절
+    /// </summary>
+    /// <param name="volume"></param>
+    public void SetVolumeSFX(float volume)
+    {
+        _sfx.volume = volume;
+    }
+}
