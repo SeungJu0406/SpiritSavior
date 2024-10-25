affa06170f59b68654e3794b6e8827bd545f788b
KanghyeokYeo <ykh5548@naver.com>
2024-10-25 20:06:39 +0900

    chore : thorn script

--- Assets/Project/YeoKanghyeok/Scripts/OnCollisionPlayer.cs
+++ Assets/Project/YeoKanghyeok/Scripts/OnCollisionPlayer.cs
@@ -1,14 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class OnCollisionPlayer : MonoBehaviour
 {
+    [SerializeField] float xPower;
+    [SerializeField] float yPower;
+    Transform transform;
+    private void Start()
+    {
+        transform = GetComponent<Transform>();
+    }
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            Destroy(collision.gameObject);
+            if(collision.transform.position.x - transform.position.x<0)
+            {
+                collision.rigidbody.velocity = Vector2.left*xPower;
+            }
+            else
+            {
+                collision.rigidbody.velocity = Vector2.right * xPower;
+            }
+            collision.rigidbody.velocity = Vector2.up * yPower;
         }
     }
 }
