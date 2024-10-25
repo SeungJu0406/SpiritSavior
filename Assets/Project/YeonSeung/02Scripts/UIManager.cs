c058a260a99d147cad039c11f07dd95d39ac4007
YoungsonMan <choi13570@naver.com>
2024-10-25 10:25:32 +0900

    [Fix] 피통 / 목숨 +- 기능 수정
    +- 조금 이상하게 되던거 수정
                lives[life].SetActive (true);
                life += 1;
    액티브 먼저하고 더하기
    
    테스트 버튼 구현

--- Assets/Project/YeonSeung/02Scripts/UIManager.cs
+++ Assets/Project/YeonSeung/02Scripts/UIManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+// using UnityEngine.UIElements;
 
 public class UIManager : MonoBehaviour
 {
@@ -16,7 +17,42 @@ public class UIManager : MonoBehaviour
 
     [Header("Live Count")]
     [SerializeField] GameObject[] lives;
-    [SerializeField] public int life; 
+    [SerializeField] public int life;
+    [SerializeField] public int maxLife;
+
+
+    public void GiveDamage()
+    {
+        curHp -= 33;
+        // life -= 1;
+    }
+
+    public void GiveLife()
+    {
+        // HP BAR
+        if(curHp >= 100)
+        {
+            curHp = 100;
+        }
+        else
+        {
+            curHp += 33;
+
+        }
+
+        // LIFE
+        if(life >= maxLife)
+        {
+            life = maxLife;
+            lives[life].SetActive (true);
+        }
+        else
+        {
+            
+            lives[life].SetActive (true);
+            life += 1;
+        }
+    }
 
 
     // 데미지 받는함수
@@ -26,8 +62,9 @@ public class UIManager : MonoBehaviour
         {
             life -= damage;
 
-            Destroy(lives[life].gameObject);
-            if(life < 1)
+            //Destroy(lives[life].gameObject);
+            lives[life].SetActive(false);
+            if (life < 1)
             {
                 _isDead = true;
             }
@@ -56,6 +93,7 @@ public class UIManager : MonoBehaviour
 
     void Start()
     {
+        maxLife = lives.Length;
         life = lives.Length;
         curHp = maxHp;
     }
