dc45a5585d27513a64ca34266bb0d6debe444a81
KanghyeokYeo <ykh5548@naver.com>
2024-10-25 16:34:41 +0900

    chore : add private name underbar
    chore : add private name underbar

--- Assets/Project/YeoKanghyeok/Scripts/TrampolineController.cs
+++ Assets/Project/YeoKanghyeok/Scripts/TrampolineController.cs
@@ -2,19 +2,19 @@ using UnityEngine;
 
 public class TrampolineController : MonoBehaviour
 {
-    [SerializeField] private float jumpSpeed;
-    Animator animator;
+    [SerializeField] private float _jumpSpeed;
+    private Animator _animator;
 
     private void Start()
     {
-        animator = GetComponent<Animator>();
+        _animator = GetComponent<Animator>();
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-                collision.rigidbody.velocity = Vector2.up * jumpSpeed;
-                animator.SetTrigger("boing");
+                collision.rigidbody.velocity = Vector2.up * _jumpSpeed;
+                _animator.SetTrigger("boing");
         }
     }
 }
