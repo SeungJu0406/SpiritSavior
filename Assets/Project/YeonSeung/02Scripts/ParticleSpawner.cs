b24ccf54da1c33ee5c27a2105aedee7a8c643be4
YoungsonMan <choi13570@naver.com>
2024-10-28 16:29:27 +0900

    [Feat] ObjectPool틀 추가
    더 해야됨

---
+++ Assets/Project/YeonSeung/02Scripts/ParticleSpawner.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSpawner : MonoBehaviour
+{
+   // public ObjectPool<ParticleSystem> particlePool;
+   // void Start()
+   // {
+   //
+   //     
+   // }
+
+}
