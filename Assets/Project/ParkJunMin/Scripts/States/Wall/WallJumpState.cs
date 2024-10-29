e9de9f5731f2941ce11a5a90c4a098401af1d498
LemoZun <marrzun@gmail.com>
2024-10-29 14:18:32 +0900

    chore : WallJump 조정중
    WallCheck 및 WallJump Power 값 조정중입니다

--- Assets/Project/ParkJunMin/Scripts/States/Wall/WallJumpState.cs
+++ Assets/Project/ParkJunMin/Scripts/States/Wall/WallJumpState.cs
@@ -4,19 +4,29 @@ using UnityEngine;
 
 public class WallJumpState : PlayerState
 {
+    float wallJumpPower;
     public WallJumpState(PlayerController player) : base(player)
     {
         animationIndex = (int)PlayerController.State.WallJump;
+        wallJumpPower = player.wallJumpPower;
     }
     public override void Enter()
     {
         player.playerView.PlayAnimation(animationIndex);
+        player.rigid.velocity = new Vector2(-player.isPlayerRight, 0.9f * wallJumpPower); //0.9는 점프조절 임시값
+        player.playerView.FlipRender(player.isPlayerRight);
         
     }
 
     public override void Update()
     {
-        PlayAnimationInUpdate();
+        //player.playerView.IsAnimationFinished()
+
+        //PlayAnimationInUpdate();
+
+        //player.MoveInAir();
+
+
     }
 
     public override void Exit()
