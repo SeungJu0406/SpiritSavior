1b125f72c60f78481afce37f903fc707c467d981
YoungsonMan <choi13570@naver.com>
2024-10-29 09:37:55 +0900

    [Feat] ParticleManager 추가
    싱글톤

--- Assets/Project/YeonSeung/02Scripts/ParticleManager.cs
+++ Assets/Project/YeonSeung/02Scripts/ParticleManager.cs
@@ -4,13 +4,48 @@ using UnityEngine;
 
 public class ParticleManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public static ParticleManager Instance;
+
+    [SerializeField] GameObject runFX;
+    [SerializeField] GameObject jumpFX;
+    [SerializeField] GameObject dJumpFX;
+    [SerializeField] GameObject hitFX;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else Destroy(gameObject);
+    }
+
+
+    public void PlayRunFX()
+    {
+        ObjectPool.SpawnObject(runFX, transform.position, transform.rotation, ObjectPool.PoolType.ParticleSystem);
+    }
+    public void PlayJumpFX()
+    {
+        ObjectPool.SpawnObject(runFX, transform.position, transform.rotation, ObjectPool.PoolType.ParticleSystem);
+    }
+    public void PlayDoubleJumpFX()
+    {
+        ObjectPool.SpawnObject(runFX, transform.position, transform.rotation, ObjectPool.PoolType.ParticleSystem);
+    }
+    public void PlayHitFX()
+    {
+        ObjectPool.SpawnObject(runFX, transform.position, transform.rotation, ObjectPool.PoolType.ParticleSystem);
+    }
+
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+
     void Update()
     {
         
