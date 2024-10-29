611b403197780221b8f3b16174003774137144f1
YoungsonMan <choi13570@naver.com>
2024-10-29 21:05:26 +0900

    [Feat] MovingPLatform2 추가
    밟으면 움직이고 A->B 지점 움직이는 스타일
    
    일정시간후 움직이는거 구현중

--- Assets/Project/YeonSeung/02Scripts/MovingPlatformV2.cs
+++ Assets/Project/YeonSeung/02Scripts/MovingPlatformV2.cs
@@ -2,54 +2,87 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovingPlatformV2 : MonoBehaviour
+public class MovingPlatformV2 : SwichInteractable
 {
+    [Header("시작점")]
     public Transform pointA;
+    [Header("도착점")]
     public Transform pointB;
+    [Header("이동속도")]
     [SerializeField] float moveSpeed;
-
-   // [SerializeField] GameObject platform;
-   // Rigidbody2D rigid;
+    [Header("기다리는 시간")]
+    [SerializeField] float delay;
 
     private Vector3 nextPosition;
+
+    private bool _isMoving;
+
+
+    private Coroutine _delayMove;
+
+    public override void Interact()
+    {
+        // _isMoving이 활성화 안되면 아마 안될것
+        // 그전에 switch되나 확인차 했던것.
+        MovePlatform();
+    }
+
+    private void Awake()
+    {
+        _isMoving = false;
+    }
     void Start()
     {
         nextPosition = pointB.position;
-        
     }
 
 
     void Update()
     {
-        
-        // transform.position = Vector3.MoveTowards(transform.position, nextPosition, moveSpeed * Time.deltaTime);
-        // // A로 복귀
-        // if(transform.position == nextPosition )
-        // {
-        //     nextPosition = (nextPosition == pointA.position) ? pointB.position : pointA.position;
-        // }
+        // _isMoving이 활성화때만 움직이기
+        if (_isMoving == true)
+        {
+            if (_delayMove == null)
+            {
+                _delayMove = StartCoroutine(DelayMove());
+                MovePlatform();
+            }
+            else if (_delayMove != null)
+            {
+                StopCoroutine(_delayMove);
+                Debug.Log("코루틴끝");
+                _delayMove = null;
+            }
+        }
+        else if (_isMoving == false)
+        {
+            RetreatPlatform();
+        }
+
+    }
+    IEnumerator DelayMove()
+    {
+        Debug.Log("Coroutine STARTS!");
+        yield return new WaitForSeconds(delay);
+        Debug.Log($"{delay}후 Coroutine 끝");
     }
 
     public void MovePlatform()
     {
         transform.position = Vector3.MoveTowards(transform.position, nextPosition, moveSpeed * Time.deltaTime);
-        // A로 복귀
-        if (transform.position == nextPosition)
-        {
-            nextPosition = (nextPosition == pointA.position) ? pointB.position : pointA.position;
-        }
     }
-    private void GoPlatform()
+    public void RetreatPlatform()
     {
-        
+        // A(시작점)로 복귀
+        transform.position = Vector3.MoveTowards(transform.position, pointA.position, moveSpeed * Time.deltaTime);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            
-            collision.gameObject.transform.parent = transform;
+            _isMoving = true;
+            collision.transform.SetParent(transform);
         }
     }
     private void OnCollisionExit2D(Collision2D collision)
@@ -57,6 +90,8 @@ public class MovingPlatformV2 : MonoBehaviour
         if (collision.gameObject.tag == "Player")
         {
             collision.gameObject.transform.parent = null;
+            _isMoving = false;
+            Debug.Log(_isMoving);
         }
     }
 }
