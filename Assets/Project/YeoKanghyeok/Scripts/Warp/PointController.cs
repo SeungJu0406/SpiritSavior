b4e41a74fe7291bf5ffb8dfa5cda802ad26ff207
KanghyeokYeo <ykh5548@naver.com>
2024-10-31 18:08:58 +0900

    feat : particle system 필드화
    feat : particle system 필드화

--- Assets/Project/YeoKanghyeok/Scripts/Warp/PointController.cs
+++ Assets/Project/YeoKanghyeok/Scripts/Warp/PointController.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class PointController : MonoBehaviour
@@ -7,22 +6,21 @@ public class PointController : MonoBehaviour
     [SerializeField] GameObject buttonPrefab;
     [SerializeField] GameObject buttonCanvas;
     [SerializeField] Transform buttonLayout;
-    [SerializeField] GameObject unActivePrefab;
-    [SerializeField] GameObject activePrefab;
-    [SerializeField] bool _inPoint; // point 접근여부
+    [SerializeField] ParticleSystem unActiveParticle;
+    [SerializeField] ParticleSystem activeParticle;
     [SerializeField] bool _pointActive; // point 활성화 여부
     [SerializeField] bool _uiActive; // ui canvas 활성화 여부
     private GameObject _buttonObject;
-    private GameObject _pointObject;
+    public ParticleSystem ps;
+    public ParticleSystem.MainModule psMain;
     public ButtonController _button;
     private Transform _transform;
     void Start()
     {
-        _inPoint = false;
         _pointActive = false;
 
-        _pointObject = GetComponent<GameObject>();
-        _pointObject = unActivePrefab;
+        psMain = ps.main;
+        ps = unActiveParticle;
 
         buttonCanvas.SetActive(false);
 
@@ -36,18 +34,16 @@ public class PointController : MonoBehaviour
     {
         if (collision.gameObject.tag == "Player")
         {
-            //_inPoint = true;
-            _inputRoutine = _inputRoutine==null ? StartCoroutine(InputRoutine()) : _inputRoutine;
-        }       
+            _inputRoutine = _inputRoutine == null ? StartCoroutine(InputRoutine()) : _inputRoutine;
+        }
     }
 
-    
+
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            //_inPoint = false;
             if (_inputRoutine != null)
             {
                 StopCoroutine(_inputRoutine);
@@ -60,7 +56,6 @@ public class PointController : MonoBehaviour
     }
     #endregion
 
-
     Coroutine _inputRoutine;
     IEnumerator InputRoutine()
     {
@@ -68,14 +63,13 @@ public class PointController : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.F))
             {
-                // point활성화 및 button 생성
                 if (!_pointActive)
                 {
                     ActivePoint();
                     _pointActive = true;
                 }
 
-                // ui canvas 활성화
+
                 if (!_uiActive)
                 {
                     buttonCanvas.SetActive(true);
@@ -91,25 +85,13 @@ public class PointController : MonoBehaviour
         }
     }
 
-
-    void Update()
-    {
-        if (_inPoint)
-        {
-           
-        }
-        else
-        {
-
-        }
-    }
-
     private void ActivePoint()
     {
-        _pointObject = activePrefab;
+        ps = activeParticle;
 
         _buttonObject = Instantiate(buttonPrefab, buttonLayout) as GameObject; // button 생성
         _button = _buttonObject.GetComponent<ButtonController>();
+        _button.Destinasion = _transform;
         _button.Destinasion = _transform;
         _button.Destinasion.position = _transform.position; // point 위치 버튼 목적지에 대입
     }
