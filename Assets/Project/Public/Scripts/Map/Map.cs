8e93fe9132fc9e742e311e9fdf1bbca4498e9d3a
EUSUNGKIM <jimmy0818@naver.com>
2024-10-31 21:23:38 +0900

    [Fix] 지도 시스템에서 빨간색 파란색 구현
    빨간색 캐릭터면 지도상 빨강으로 나오고 파란색이면 파랑으로 나옴

--- Assets/Project/Public/Scripts/Map/Map.cs
+++ Assets/Project/Public/Scripts/Map/Map.cs
@@ -4,18 +4,49 @@ using UnityEngine;
 
 public class Map : MonoBehaviour
 {
+
+    public GameObject redFace;
+    public GameObject blueFace;
     public GameObject mapImage;
 
+    private PlayerController _playerController;
     void Start()
     {
         mapImage.SetActive(false);
-    }
+        redFace.SetActive(false);
+        blueFace.SetActive(false);
 
+        _playerController = FindObjectOfType<PlayerController>();
+    }
+    public void FaceMap()
+    {
+        if (_playerController != null)
+        {
+            // 색상 확인
+            if (_playerController.playerModel.curNature == PlayerModel.Nature.Red)
+            {
+                redFace.SetActive(true);
+                blueFace.SetActive(false);
+            }
+            else if (_playerController.playerModel.curNature == PlayerModel.Nature.Blue)
+            {
+                redFace.SetActive(false);
+                blueFace.SetActive(true);
+            }
+            
+        }
+    }
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.M))
         {
             mapImage.SetActive(!mapImage.activeSelf);
+            if (!mapImage.activeSelf)
+            {
+                redFace.SetActive(false);
+                blueFace.SetActive(false);
+            }
+            FaceMap();
         }
     }
 }
