25ed1920849ee80454539795e63371eee8f7192b
YoungsonMan <choi13570@naver.com>
2024-11-01 04:59:05 +0900

    [Feat] 번개 추가
    SetTrigger - OnTirggerEnter2D
    충돌 & (지정한)데미지
    
    색구별 충돌판정 구현 해야함
    
    발판에 색깔로  힌트를 주면 어떨까해서 넣어볼까함

--- Assets/Project/YeonSeung/02Scripts/LightningController.cs
+++ Assets/Project/YeonSeung/02Scripts/LightningController.cs
@@ -8,12 +8,16 @@ public class LightningController : MonoBehaviour
     [SerializeField] GameObject LightningB;
     [Header("빨간번개")]
     [SerializeField] GameObject LightningR;
+    [Header("힌트 파란")]
+    [SerializeField] GameObject glimpseB;
+    [Header("힌트 빨간")]
+    [SerializeField] GameObject glimpseR;
     [Header("번개 칠 위치")]
     [SerializeField] Transform hitSpot;
     [Header("번개 치는 주기")]
     [SerializeField] float hittingPeriod;
-    [Header("번개 치는 주기")]
-    [SerializeField] float strikeSpeed;
+   // [Header("번개 치는 주기")]
+   // [SerializeField] float strikeSpeed;  // 스피드가 필요한가 싶은 느낌? 그냥 닿으면 죽는거야~
     [Header("번개 데미지")]
     [SerializeField] int lightningDamage;
     [Header("번개 랜덤")]
@@ -21,7 +25,11 @@ public class LightningController : MonoBehaviour
 
 
     [SerializeField] PlayerModel.Nature _lightingNature;
+    // 파 - 0
+    // 빨 - 1
+    private GameObject _thisStrike;
 
+    // OnTriggerEnter2D 사용
 
     PlayerController _player;
 
@@ -47,12 +55,17 @@ public class LightningController : MonoBehaviour
         _player = Manager.Game.Player;
         _player.playerModel.OnPlayerTagged += SetActiveCollider;
 
+        
     }
 
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            ObjectPool.SpawnObject(LightningB, hitSpot.position, transform.rotation, ObjectPool.PoolType.ParticleSystem);
+        }
+
     }
 
 
@@ -61,6 +74,10 @@ public class LightningController : MonoBehaviour
      * Tag 색상 == 번개색상, 피해받음
      */
 
+    private void Lightning()
+    {
+        ObjectPool.SpawnObject(LightningB, hitSpot.position, transform.rotation, ObjectPool.PoolType.ParticleSystem);
+    }
     private void SetActiveCollider(PlayerModel.Nature nature)
     {
         if (nature == _lightingNature)
@@ -68,11 +85,29 @@ public class LightningController : MonoBehaviour
             
         }
     }
-    private void OnCollisionEnter2D(Collision2D collision)
+ //   private void OnCollisionEnter2D(Collision2D collision)
+ //   {
+ //       if (collision.gameObject.tag == "Player")
+ //       {
+ //           _player.playerModel.TakeDamageEvent(lightningDamage);
+ //       }
+ //   }
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
+            Debug.Log("OnTriggerEnter TEST");
+            Lightning();
             _player.playerModel.TakeDamageEvent(lightningDamage);
         }
     }
+
+    private void GenerateRandom()
+    {
+        int _chance = Random.Range(0, 1);
+        if (_chance == 0)
+            _thisStrike = LightningB;
+            else _thisStrike = LightningR;
+    }
+
 }
