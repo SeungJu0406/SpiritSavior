02804fd8d1ed89419931d926d82fa24d0d5428a9
YoungsonMan <choi13570@naver.com>
2024-11-01 13:54:45 +0900

    [Fix] 라이트닝 분리
    주기적으로 낙뢰 생성하는 것 empty오브젝트 두고
    번개에다가 충돌해서 막거나 데미지주는 것 따로 붙임
    근데 뭔가  콜라이더 작동하자가 생겼습니다.

--- Assets/Project/YeonSeung/02Scripts/LightningController.cs
+++ Assets/Project/YeonSeung/02Scripts/LightningController.cs
@@ -14,18 +14,25 @@ public class LightningController : MonoBehaviour
     [SerializeField] GameObject glimpseR;
     [Header("번개 칠 위치")]
     [SerializeField] Transform hitSpot;
+    [Space(10)]
+
     [Header("번개 치는 주기:  __초 마다 콰광")]  // 근데 
     [SerializeField] float hittingPeriod;
+   // [SerializeField] bool _canRespawn;
 
     [Header("번개 데미지")]
     [SerializeField] int lightningDamage;
-    [Header("번개 랜덤")]
-    [SerializeField] bool isRandom;
+  // 
+  //
+  //  [SerializeField] Collider2D _hitBox;
+
 
 
     [SerializeField] PlayerModel.Nature _lightingNature;
-    
-    // 파 - 0 : 빨 - 1
+
+    // 파 - 0 : 빨 - 1  / 나중에 물어보고 다른스타일 기믹하면
+    [Header("번개 랜덤")]
+    [SerializeField] bool isRandom;
     private GameObject _thisStrike;
 
     // OnTriggerEnter2D 사용
@@ -38,24 +45,27 @@ public class LightningController : MonoBehaviour
 
     private bool _canAttack = true;
 
-    private Coroutine _delayTime;
+    private Coroutine _PeriodicStrike;
 
+    WaitForSeconds _respawnDelay;
+    WaitForSeconds _poolDelay;
+    float _poolDelayTime = 0.5f;
 
  // Tag 색상 != 번개색상, 회피가능
  // Tag 색상 == 번개색상, 피해받음
 
 
 
-    IEnumerator DelayStrike()
-    {
-        yield return new WaitForSeconds(hittingPeriod);
-    }
+    
 
     void Awake()
     {
         // Layer 추가
         _defaultLayer = gameObject.layer;
         _ignorePlayerLayer = LayerMask.NameToLayer("Ignore Player");
+        _respawnDelay = new WaitForSeconds(hittingPeriod);
+        _poolDelay = new WaitForSeconds(_poolDelayTime);
+      //  _hitBox = GetComponent<Collider2D>();
     }
     void Start()
     {
@@ -66,6 +76,7 @@ public class LightningController : MonoBehaviour
         _player = Manager.Game.Player;
         _player.playerModel.OnPlayerTagged += SetActiveCollider;
         SetActiveCollider(_player.playerModel.curNature);
+      //  _hitBox.enabled = false;
     }
     private void OnEnable()
     {
@@ -74,6 +85,48 @@ public class LightningController : MonoBehaviour
         _player.playerModel.OnPlayerTagged += SetActiveCollider;
         SetActiveCollider(_player.playerModel.curNature);
     }
+
+    private void Update()
+    {
+       if(Input.GetKeyDown(KeyCode.Q))
+        {
+            Lightning();
+        }
+
+        if (_PeriodicStrike == null)
+        {
+            _PeriodicStrike = StartCoroutine(RespawnRoutine());
+            Debug.Log("코루틴끝");
+         //   Lightning();
+        }
+       // else if (_PeriodicStrike != null)
+       // {
+       //     StopCoroutine(_PeriodicStrike);
+       //     
+       // }
+       // StopCoroutine 구현해야됨
+        
+    }
+    IEnumerator RespawnRoutine()
+    {
+       while(true)
+        {
+            Debug.Log("Coroutine번개");
+            Lightning();
+            yield return _respawnDelay;
+
+        }
+       
+    }
+    private void BoxOn()
+    {
+      //  _hitBox.enabled = true;
+    }
+    private void BoxOff()
+    {
+       // _hitBox.enabled = false;
+    }
+
     private void SetActiveCollider(PlayerModel.Nature nature)
     {
         if (nature == _lightingNature)
@@ -97,11 +150,13 @@ public class LightningController : MonoBehaviour
             if(_canAttack)
             {
                 Debug.Log("OnTriggerEnter TEST");
-                Lightning();
+                // Lightning();
                 _player.playerModel.TakeDamageEvent(lightningDamage);
             }
         }
     }
+    
+
 
 
     private void GenerateRandom()
