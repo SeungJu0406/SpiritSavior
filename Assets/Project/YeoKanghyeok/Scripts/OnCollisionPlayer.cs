6ee80365e8b9bb04d81503fe773848fbc994beba
KanghyeokYeo <ykh5548@naver.com>
2024-11-04 14:04:54 +0900

    fix : 가시함정 버그수정

--- Assets/Project/YeoKanghyeok/Scripts/OnCollisionPlayer.cs
+++ Assets/Project/YeoKanghyeok/Scripts/OnCollisionPlayer.cs
@@ -2,37 +2,13 @@ using UnityEngine;
 
 public class OnCollisionPlayer : MonoBehaviour
 {
-    [SerializeField] float xPower;
-    [SerializeField] float yPower;
-    [SerializeField] PlayerController _player;
     [SerializeField] int attackPower = 1;
-    Transform transform;
-    private void Start()
-    {
-        GameObject playerObject = GameObject.FindWithTag("Player");
-        _player = playerObject.GetComponent<PlayerController>();
-    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        _player.playerModel.TakeDamageEvent(attackPower);
+        if (collision.gameObject == Manager.Game.Player.gameObject)
+        {
+            Manager.Game.Player.playerModel.TakeDamageEvent(attackPower);
+        }
     }
-
-    //private void OnCollisionEnter(Collision collision)
-    //{
-    //    _player.playerModel.TakeDamage(attackPower);
-    //}
-
-    //if (collision.gameObject.tag == "Player")
-    //{
-    //    if(collision.transform.position.x - transform.position.x<0)
-    //    {
-    //        collision.rigidbody.velocity = Vector2.left*xPower;
-    //    }
-    //    else
-    //    {
-    //        collision.rigidbody.velocity = Vector2.right * xPower;
-    //    }
-    //    collision.rigidbody.velocity = Vector2.up * yPower;
-    //}
 }
 
