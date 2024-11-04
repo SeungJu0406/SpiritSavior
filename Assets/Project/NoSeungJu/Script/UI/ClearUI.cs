94c5123bf49a5a61eb7a1de0948d96de1c155fd4
SeungJu0406 <nss8164@gmail.com>
2024-11-04 15:37:27 +0900

    feat : 클리어 UI 초안 구현
    크레딧, 파티클 등 퀄리티 거리 남아있음

--- Assets/Project/NoSeungJu/Script/UI/ClearUI.cs
+++ Assets/Project/NoSeungJu/Script/UI/ClearUI.cs
@@ -1,9 +1,44 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ClearUI : BaseUI
 {
+    PlayableDirector _timelineDirector;
 
+    protected override void Awake()
+    {
+        base.Awake();
+        _timelineDirector = GetComponent<PlayableDirector>();
+    }
+    private void OnEnable()
+    {
+        _timelineDirector.Play();
+    }
 
+    private void Start()
+    {
+        Init();
+        SubscribesEvents();
+    }
+
+    void BackTitleScene()
+    {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadSceneAsync("TitleScene");
+    }
+
+    private void SubscribesEvents()
+    {
+        GetUI<Button>("MainButton").onClick.AddListener(BackTitleScene);
+    }
+
+    private void Init()
+    {
+        
+    }
 }
