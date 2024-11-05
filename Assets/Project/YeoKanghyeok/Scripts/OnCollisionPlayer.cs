bc654da2330f8a234bf36a54890c0888cec0c7fd
SeungJu0406 <nss8164@gmail.com>
2024-11-05 21:28:23 +0900

    fix : 가시 데미지 조건 수정
    Enter에서 한번 주는것이 아닌 코루틴으로 트리거 영역 내에있을때 계속 데미지를 주게 바꿈

--- Assets/Project/YeoKanghyeok/Scripts/OnCollisionPlayer.cs
+++ Assets/Project/YeoKanghyeok/Scripts/OnCollisionPlayer.cs
@@ -1,13 +1,38 @@
+using System.Collections;
 using UnityEngine;
 
 public class OnCollisionPlayer : MonoBehaviour
 {
     [SerializeField] int attackPower = 1;
-    private void OnTriggerStay2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject == Manager.Game.Player.gameObject)
         {
+            if(_hitDamagePlayer == null)
+                _hitDamagePlayer = StartCoroutine(HitDamagePlayer());
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject == Manager.Game.Player.gameObject)
+        {
+           if(_hitDamagePlayer != null)
+            {
+                StopCoroutine(_hitDamagePlayer);
+                _hitDamagePlayer = null;
+            }
+        }
+    }
+
+    WaitForSeconds delay = new WaitForSeconds(0.2f);
+    Coroutine _hitDamagePlayer;
+    IEnumerator HitDamagePlayer()
+    {
+        while (true)
+        {
             Manager.Game.Player.playerModel.TakeDamageEvent(attackPower);
+            yield return delay;
         }
     }
 }
