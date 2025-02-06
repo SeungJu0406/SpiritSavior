40e91893747ee3e423a574680ced52cfbb5d7823
LemoZun <marrzun@gmail.com>
2025-02-06 10:28:59 +0900

    chore : 주석 삭제

--- Assets/Project/ParkJunMin/Scripts/States/BaseState.cs
+++ Assets/Project/ParkJunMin/Scripts/States/BaseState.cs
@@ -2,9 +2,9 @@ namespace Project.ParkJunMin.Scripts.States
 {
     public class BaseState
     {
-        public virtual void Enter() { } // 상태 시작
-        public virtual void Update() { } // 상태 동작중
-        public virtual void Exit() { } // 상태 마무리
+        public virtual void Enter() { }
+        public virtual void Update() { }
+        public virtual void Exit() { }
         public virtual void FixedUpdate() { }   
     }
 }
